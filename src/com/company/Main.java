745160ba90fa04523d585ff13dfdbd86547e6632
Leo Sinani <kajayr_sinanian@yahoo.com>
2021-05-04 11:19:26 -0700

    Added try catch to my code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,20 +11,35 @@ public class Main {
         int count = 0;
         System.out.println("The computer will \"think\" of a secret number from 1 to 20 and ask the user to guess it.\n After each guess, the computer will tell the user whether the number is too high or too low.\n The user wins if they can guess the number within six tries.");
 while(count < 6){
-    int input = scanner.nextInt();
-    if(input > 20 || input < 0){
-        System.out.println("The number is out of boundary. Please select a number between 1 and 20");
-        continue;
-    }
-    count++;
-    if(randomNumber < input){
-        System.out.println("The number is too high");
-    }else if(randomNumber > input){
-        System.out.println("The number is too low");
-    }else if(input == randomNumber){
-        System.out.println("YOU WON!");
+    int input;
+
+    try{
+        input = scanner.nextInt();
+        if(input > 20 || input < 0){
+            System.out.println("The number is out of boundary. Please select a number between 1 and 20");
+            continue;
+        }
+        count++;
+        if(randomNumber < input){
+            System.out.println("The number is too high");
+        }else if(randomNumber > input){
+            System.out.println("The number is too low");
+        }else if(input == randomNumber){
+            System.out.println("YOU WON!");
+            break;
+        }
+
+    }catch(InputMismatchException e){
+        System.out.println("This is the InputMismatchException E");
+        System.out.println(e.getMessage());
         break;
     }
+    catch(Exception e){
+        System.out.println("This is the general E");
+        System.out.println(e.getMessage());
+        break;
+    }
+
     System.out.println("\nYou guessed wrong for 6 times! Game over");
 }
 
